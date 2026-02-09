0ff4e63f708e4f97ef597c2a17c8c1bdd7d60d74
amillyahnc <amillyahnc@nycstudents.net>
2026-02-09 14:00:40 +0000

    update practicals

--- WDLesson3_7Practical/script.js
+++ WDLesson3_7Practical/script.js
@@ -3,7 +3,11 @@
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
-
+function interest(){
+      let p = parseFloat(document.getElementById("p").value);
+      let 2d = parseFloat(document.getElementById("2d").value);
+      let output = document.getElementById("output");
+}
 /* Challenge 4: Create a function to serve as the event handler for the compound interest UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
       2) Perform the necessary calculations
---
+++ WDLesson4_2Practical/script.js
@@ -0,0 +1,48 @@
+// General Guideline
+/*
+function name(){
+  // Step 1: Create variables for the information from the text inputs and the output element
+
+  // Step 2: Complete the process
+  //    a) Perform any calculations necessary
+  //    b) Perform necessary decisions
+
+  // Step 3: Display the output
+
+}
+*/
+
+/* Challenge 2:  Create the function for Challenge 1 scenario */
+function area(){
+       let l = parseFloat(document.getElementById("l").value);
+       let w = parseFloat(document.getElementById("w").value);
+       let output = document.getElementById("output"); 
+       let msg = ""
+       let a = l * w
+       if( l<= 0 || w<= 0){
+        msg="Innapropriate measurement";
+}
+else{
+  
+  msg = `Area is ${a}`;
+}
+output.innerHTML = msg;
+/* Challenge 3:  Complete the function for Pets Challenge on pets.html.  
+1) Review the HTML for the UI.  Remember dropdown boxes are just input.
+2) You will need to write HTML as part of the output.  
+   For example, output.innerHTML = `<input src="pic.png">` would place the image in the <div>*/
+function pet(){
+  let animal = document.getElementById("animal").value;
+  let feeling = document.getElementById("emotion").value;
+  let output = document.getElementById("output");
+  let picture = "";
+
+  if(animal =="Bear" && feeling =="Sad"){
+    src="sadBear.jpg";
+  }
+  if(animal =="Bear" && feeling=="Funny"){
+    src="funnyBear.jpg"
+  }
+
+  output.innerHTML = `img_src=${picture}`
+}
