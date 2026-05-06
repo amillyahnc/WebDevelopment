71ec727e23b4b9b836ba277fc16342074d06a95f
amillyahnc <amillyahnc@nycstudents.net>
2026-05-06 13:31:23 +0000

    update practicals

--- WDLesson6_7Practical/script.js
+++ WDLesson6_7Practical/script.js
@@ -1,14 +1,14 @@
 let data;
 
   async function init(){
-let link ="collision.json";
+  let link ="collision.json";
   info = await fetch(link);
   data = await info.json();
 
-  let output = document.getElementById("output");
+  output = document.getElementById("output");
   let build = "";
-
-  for(let i = 0; i < data.length; i+=1){
+  }
+  for(let i = 0; i < data.length; i+= 1){
     let collision = data[i];
     build += `<div class="fitted card">`
     
@@ -18,11 +18,7 @@ let link ="collision.json";
   <p> Street Name: ${collision.street_name}</p>;
   <p> Borough: ${collision.borough}</p>;
   <p> Injured: ${collision.number_of_persons_injured}</p>;
-
-</div>` 
-}
-   output.innerHTML= build; 
-}
+  }
 function filterByZip(){
       let output = document.getElementById("output");
       let Zip = document.getElementById("Zipcode").value;
